f0e90c91bdd8e4af798a00f383b200dc5d380cf6
Luan Peres <delinoperes@gmail.com>
2020-12-08 15:31:12 -0300

    Project complete

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="playing">play</button>
+    <div v-if="playing">
+      <Block :delay="delay" @emitTimer="endGame" />
+    </div>
+    <Results v-if="showResults" :timer="score" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Results from "./components/Results";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      playing: false,
+      showResults: false,
+      delay: null,
+      score: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = Math.floor(2000 + Math.random() * 5000);
+      this.playing = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.playing = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+body {
+  font-family: "Roboto", sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+}
+
+button {
+  width: 200px;
+  border-radius: 20px;
+  background: #56c7ab;
+  color: #fff;
+  text-align: center;
+  margin: 1rem auto;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Your reaction is: {{ timer }} ms</h1>
+</template>
+
+<script>
+export default {
+  props: ["timer"],
+};
+</script>
+
+<style>
+</style>
